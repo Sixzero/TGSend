f3f4b8f3f5281c66967795676b419d07427cdad5
SixZero <havliktomi@hotmail.com>
2024-04-26 21:48:45 +0200

    Adding more ways to debug if something is wrong

--- test/test.jl
+++ test/test.jl
@@ -1,3 +1,12 @@
+# printing the environment variables whether to check julia can see them.
+api_id = get(ENV, "TELEGRAM_API_ID", "")
+@show api_id
+api_hash = get(ENV, "TELEGRAM_API_HASH", "")
+@show api_hash
+session_string = get(ENV, "TELEGRAM_SESSION_STRING", "")
+@show session_string
+;
+#%%
 using TGSend: app, send_msg
 @show app
 send_msg()
